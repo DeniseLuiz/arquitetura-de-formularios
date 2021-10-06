73dcd71540afbc9c031f1979d93c60503111cd68
DeniseLuiz <denisesantos.luiz@usp.br>
2021-10-06 17:50:10 -0300

    login template

--- gatitobook/src/app/app-routing.module.ts
+++ gatitobook/src/app/app-routing.module.ts
@@ -2,7 +2,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from "@angular/core";
 
 const routes: Routes = [
-  {}
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m)=> m.HomeModule)
+  }
 ]
 
 @NgModule({
--- gatitobook/src/app/home/home-routing.module.ts
+++ gatitobook/src/app/home/home-routing.module.ts
@@ -1,7 +1,21 @@
+import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      //children é um array de rotas
+      {
+        path: '',
+        component: LoginComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
