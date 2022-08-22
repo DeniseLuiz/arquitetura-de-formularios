34c491fb8624836b57b8d1bc38cbd2656dcbaf78
DeniseLuiz <denisesantos.luiz@usp.br>
2022-08-21 21:23:27 -0300

    feat: cadastro de novo usuário

--- gatitobook/src/app/home/home.module.ts
+++ gatitobook/src/app/home/home.module.ts
@@ -5,18 +5,21 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NovoUsuarioComponent
   ],
   imports: [
     CommonModule,
     HomeRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     MensagemModule
   ],
   exports: [
---
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,17 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { NovoUsuario } from './novo-usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NovoUsuarioService {
+
+  urlBase = 'http://localhost:3000';
+  constructor(private http: HttpClient) { }
+
+  cadastraNovoUsuario(novoUsuario: NovoUsuario): Observable<any>{
+    return this.http.post(`${this.urlBase}/user/signup`, novoUsuario)
+  }
+}
