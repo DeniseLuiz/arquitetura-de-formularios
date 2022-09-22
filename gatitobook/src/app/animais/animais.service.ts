0ba68ae38f8f9709f7e247e057c0f2c26cec49fc
DeniseLuiz <denisesantos.luiz@usp.br>
2022-09-22 13:59:45 -0300

    feat: funcionalidade like e exclusão

--- gatitobook/src/app/animais/animais.service.ts
+++ gatitobook/src/app/animais/animais.service.ts
@@ -1,10 +1,13 @@
 import { environment } from './../../environments/environment.prod';
 import { Animais, Animal } from './animais';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, mapTo } from 'rxjs/operators';
 
 const API = environment.apiUrl;
+const NOT_MODIFIED = '304';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -17,7 +20,22 @@ export class AnimaisService {
     return this.http.get<Animais>(`${API}/${userName}/photos`);
   }
 
-  getAnimalId(id: number): Observable<Animal>{
+  getAnimalId(id: number): Observable<Animal> {
     return this.http.get<Animal>(`${API}/photos/${id}`);
   }
+
+  deleteAnimal(id: number): Observable<Animal> {
+    return this.http.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  like(id: number): Observable<boolean> {
+    return this.http.post(`${API}/photos/${id}/like`, {}, {observe: 'response'}).
+    pipe(
+      mapTo(true),
+      catchError((err) => {
+        return err.status === NOT_MODIFIED ? of(false) : of(true);
+      })
+    );
+  }
+
 }
--- gatitobook/src/app/animais/detalhe-animal/detalhe-animal.component.ts
+++ gatitobook/src/app/animais/detalhe-animal/detalhe-animal.component.ts
@@ -2,7 +2,7 @@ import { AnimaisService } from './../animais.service';
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { Animal } from '../animais';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-detalhe-animal',
@@ -10,16 +10,33 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./detalhe-animal.component.css']
 })
 export class DetalheAnimalComponent implements OnInit {
-
+  animalId!: number
   animal$!: Observable<Animal>;
 
   constructor(
     private animaisService: AnimaisService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
-    const animalId = this.activatedRoute.snapshot.params.animalId;
-    this.animal$ = this.animaisService.getAnimalId(animalId);
+    this.animalId = this.activatedRoute.snapshot.params.animalId;
+    this.animal$ = this.animaisService.getAnimalId(this.animalId);
+  }
+
+  like() {
+    this.animaisService.like(this.animalId).subscribe((like) => {
+      if(like){
+        this.animal$ = this.animaisService.getAnimalId(this.animalId);
+      }
+    })
+  }
+
+  deletePhoto() {
+    this.animaisService.deleteAnimal(this.animalId).subscribe(() => {
+      this.router.navigate(['/animais/']);
+    }, (err)=> {
+      console.log(err);
+    })
   }
 }
